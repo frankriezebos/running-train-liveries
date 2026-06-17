245fb63764bc8ff8464bca92c1a7c53983e85f48
Frank <contact@frankriezebos.nl>
2026-06-17 18:43:43 +0200

    add more reskins page

---
+++ more-reskins.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>More reskins - Liveries for Running Train</title>
+  <meta name="description" content="Where to download more reskins from other livery designers?" />
+  <meta robots="index, follow" />
+  <meta name="keywords" content="Running Train, train liveries, custom textures, game mods, train skins" />
+  <meta property="og:title" content="More reskins - Liveries for Running Train" />
+  <meta property="og:description" content="Where to download more reskins from other livery designers?" />
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <div class="container">
+    <?php include('components/header.php'); ?>
+
+    <div class="section">
+      <h1 class="h2">More reskins on the official Discord server</h1>
+
+      <p class="date">17-06-2026</p>
+
+      <p>Currently I'm the only uploader on this website, but there are a lot more liveries available to download on
+        the official Discord of RUNNING TRAIN. I myself am not affiliated with the
+        game developer or the Discord server admin. But I also post my reskins over there and there are a lot of awesome
+        reskins
+        from
+        other livery designers available to download for free!</p>
+
+      <a href="https://discord.com/invite/6cx5yrkrmp" target="_blank" rel="noreferrer noopener" class="btn discord-btn">
+        <img src="/img/discord.svg" class="discord-icon" />
+        Official
+        Discord
+      </a>
+    </div>
+
+    <?php include('components/footer.php'); ?>
+  </div>
+
+  <script src="js/server.js"></script>
+</body>
+
+</html>
